aafbbf88e321e0156d5270064cb4d689bd8c9ea6
Irene Rojas <irene@smallwondermedia.com>
2023-12-05 12:31:19 -0500

    added noopener

--- src/App.js
+++ src/App.js
@@ -188,7 +188,7 @@ function App() {
                     <p>{result}</p>
                     {showRightComponent && <Right value={randomWord} onPlayAgain={handlePlayAgain} />}
                 </div>
-                <p>Powered by Merriam-Webster <a href="https://dictionaryapi.com/" target="_blank"><img className="mwLogo" src={mwLogo} alt="Merriam-Webster Logo" /></a></p>
+                <p>Powered by Merriam-Webster <a href="https://dictionaryapi.com/" target="_blank" rel="noopener noreferrer"><img className="mwLogo" src={mwLogo} alt="Merriam-Webster Logo" /></a></p>
             </div>
         </div>
     );
